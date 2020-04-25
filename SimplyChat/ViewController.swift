67895716e59ccc1fc0aa3bf419ac6db9e6437d7d
mc-off <makov.tm@gmail.com>
2020-04-25 08:02:06 +0300

    не полностью нарисовал логин страницу. В инициал страницах (овервью) есть бага с тем, что не открываются промежуточные страница, проблема с таргет кнопки.  В остальном уже не совсем помню. Запускать желательно на Xs, 11 pro

--- SimplyChat/ViewController.swift
+++ SimplyChat/ViewController.swift
@@ -12,9 +12,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        if Core.shared.isNewUser() {
+            //show
+            guard let vc = storyboard?.instantiateViewController(identifier: "welcome") as? WelcomeViewController else {
+                return
+            }
+            vc.modalPresentationStyle = .fullScreen
+            present(vc, animated: true)
+        }
+    }
 
 }
 
+class Core {
+    static let shared = Core()
+    
+    func isNewUser() -> Bool {
+        return !UserDefaults.standard.bool(forKey: "isNewUser")
+    }
+    
+    func setIsNotNewUser() {
+        UserDefaults.standard.set(true, forKey: "isNewUser")
+    }
+}
